97484a9d48735badddc7eb427c6c86d8317224f3
SachinSingh812 <44691128+SachinSingh812@users.noreply.github.com>
2018-12-16 13:54:58 +0530

    intermediate commit
    minor changes in codes

--- key binding.py
+++ key binding.py
@@ -5,11 +5,12 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("test")
-
+id=input("Enter user id")
 img_counter = 0
 
 while True:
     ret, frame = cam.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow("test", frame)
     if not ret:
         break
@@ -21,26 +22,31 @@ while True:
         break
     elif k%256 == 2490368:
         # UPKEY pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+        #img_name = "opencv_frame_{}.png".format(img_counter)
         img_counter += 1
+        cv2.imwrite("dataSet1/User."+str(id)+"."+str(sampleNum)+".jpg",gray)
+        #cv2.imwrite(img_name, frame)
+        #print("{} written!".format(img_name))
+        #img_counter += 1
     elif k%256 == 2621440:
         # DOWNKEY pressed
         img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
+        cv2.imwrite("dataSet1/User.",frame)
+        #cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
     elif k%256 == 2424832:
         # LEFTKEY pressed
         img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
+        cv2.imwrite("dataSet1/User.",frame)
+        #cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
     elif k%256 == 255904:
         # RIGHTKEY pressed
         img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
+        cv2.imwrite("dataSet1/User.",frame)
+        #cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
 
